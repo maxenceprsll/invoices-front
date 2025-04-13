8669c686c9b590aaffad9eaa97ae95bafe454086
maxenceprsll <maxence.persello@icloud.com>
2025-04-14 01:38:13 +0200

    Add Clients component with routing and basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ClientsComponent } from './clients/clients.component';
 
 export const routes: Routes = [
-    {path: '', component: DashboardComponent, pathMatch: 'full'}
+    {path: '', component: DashboardComponent, pathMatch: 'full'},
+    {path: 'clients', component: ClientsComponent},
 ];
