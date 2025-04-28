fbbb5286cc9b48766ff462492abe3c56b08195b7
Maxence PERSELLO <maxence.persello@aprr.fr>
2025-04-28 13:07:47 +0200

    Add basic table example to the clientsComponent

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-clients',
-  imports: [],
+  imports: [
+    MatTableModule
+  ],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.scss'
 })
 export class ClientsComponent {
+  clients = [
+    {name: 'Client 1', subtitle: 'Subtitle 1', description: 'Description 1'},
+    {name: 'Client 2', subtitle: 'Subtitle 2', description: 'Description 2'},
+  ];
 
+  displayedColumns: string[] = [
+    'name', 'subtitle', 'description'
+
+  ];
 }
